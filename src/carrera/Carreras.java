2693932dd933b6d2ac7221ceba0a10dd89e0bcb1
Pedro Gordillo <pedrogordillo2510@gmail.com>
2021-06-24 13:39:15 -0600

    Agregue cambios a la clase carreras

--- src/carrera/Carreras.java
+++ src/carrera/Carreras.java
@@ -1,40 +1,44 @@
-
 package src.carrera;
 
+import src.vehiculo.Girar;
+import src.vehiculo.Vehiculo;
 
+public class Carreras extends Vehiculo implements Turbo, Girar {
 
-import src.vehiculo.*;
+    private double aceleracionTiple;
 
+    public Carreras(String nombre, String tipoCombustible, int cantidadCombustible, int cantidadPasajeros, int velocidadMaxima, double aceleracionBase) {
 
-public class Carreras extends Vehiculo implements Turbo {
-    
-     private double aceleracionTiple;
-
-    public Carreras(String tipoCombustible, int cantidadCombustible, int cantidadPasajeros, int velocidadMaxima, double aceleracionBase) {
-
-        super(tipoCombustible, cantidadCombustible, cantidadPasajeros, velocidadMaxima, aceleracionBase);
+        super(nombre, tipoCombustible, cantidadCombustible, cantidadPasajeros, velocidadMaxima, aceleracionBase);
     }
 
     @Override
     public String getInfo() {
-
-        return "Tipo de combustible: " + getTipoCombustible() + " Cantidad de galones: " + getCantidadCombustible() + " Cantidad de pasajeros: " + getCantidadPasajeros() + "Aceleracion base: " + getAceleracionBase();
+        return "Este vehiculo puede correr carreras";
     }
-
-    public void trasportarPersonas() {
-
-    }
-
-    
 
     @Override
     public void turbo(double aceleracionBase) {
-       aceleracionTiple = aceleracionBase *3 ;
+        System.out.println("Estoy implementando triplicar mi aceleracion base");
+        System.out.println("Aceleracion base inicial: " + getAceleracionBase());
+        System.out.println("Triplicando aceleracion...");
+        aceleracionTiple = aceleracionBase * 3;
         setAceleracionBase(aceleracionTiple);
+        System.out.println("Aceleracion base triplicada:" + getAceleracionBase());
     }
-    
-    public double getAceleracionTriple(){
+
+    public double getAceleracionTriple() {
         return aceleracionTiple;
+    }
+
+    @Override
+    public void girarDerecha() {
+        System.out.println("Puedo girar a la derecha en la pista de carreras");
+    }
+
+    @Override
+    public void girarIzquierda() {
+        System.out.println("Puedo girar a la izquierda en la pista de carreras");
     }
 
 }
